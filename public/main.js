de483c1f157a8bb72fa435a7a1e072de6fd1f440
Cassie Capewell <cassie.capewell@gmail.com>
2021-03-30 13:06:33 -0400

    backend for an app about planets

--- public/main.js
+++ public/main.js
@@ -1,19 +1,17 @@
 const deleteText = document.querySelectorAll('.del')
 
 Array.from(deleteText).forEach((element)=>{
-    element.addEventListener('click', deleteRapper)
+    element.addEventListener('click', deletePlanet)
 })
 
-async function deleteRapper(){
-    const sName = this.parentNode.childNodes[1].innerText
-    const bName = this.parentNode.childNodes[3].innerText
+async function deletePlanet(){
+    const planetName = this.parentNode.childNodes[1].innerText
     try{
-        const response = await fetch('deleteRapper', {
+        const response = await fetch('deletePlanet', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-              'stageNameS': sName,
-              'birthNameS': bName
+              'planetNameS': planetName,
             })
           })
         const data = await response.json()
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
+const PORT = 8000
 
 let db,
-    dbConnectionStr = 'mongodb+srv://demo:demo@cluster0.2wapm.mongodb.net/rap?retryWrites=true&w=majority',
-    dbName = 'rap'
+    dbConnectionStr = 'mongodb+srv://outerspace-cc:england3@cluster1.y3pxz.mongodb.net/Cluster1?retryWrites=true&w=majority',
+    dbName = 'planets'
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
-    
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
@@ -20,32 +20,33 @@ app.use(express.json())
 
 
 app.get('/',(request, response)=>{
-    db.collection('rappers').find().toArray()
+    db.collection('planets').find().toArray()
     .then(data => {
         response.render('index.ejs', { info: data })
     })
     .catch(error => console.error(error))
 })
 
-app.post('/addRapper', (request, response) => {
-    db.collection('rappers').insertOne(request.body)
+app.post('/addPlanet', (request, response) => {
+    db.collection('planets').insertOne(request.body)
     .then(result => {
-        console.log('Rapper Added')
+        console.log('planet added')
         response.redirect('/')
     })
     .catch(error => console.error(error))
 })
 
-app.delete('/deleteRapper', (request, response) => {
-    db.collection('rappers').deleteOne({stageName: request.body.stageNameS})
+app.delete('/deletePlanet', (request, response) => {
+    db.collection('planets').deleteOne({planetName: request.body.planetNameS})
     .then(result => {
-        console.log('Rapper Deleted')
-        response.json('Rapper Deleted')
+        console.log('planet deleted')
+        response.json('planet deleted')
     })
     .catch(error => console.error(error))
 
 })
 
-app.listen(process.env.PORT || PORT, ()=>{
+// process.env.PORT ||
+app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
