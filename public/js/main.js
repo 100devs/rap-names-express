ac1d5a9fa4054851b6ce6b1ff406f59584d1a541
Leon Noel <leon.a.noel+github@gmail.com>
2021-03-30 08:40:21 -0700

    added likes, css, and font-awesome. Working

---
+++ public/js/main.js
@@ -0,0 +1,54 @@
+const deleteText = document.querySelectorAll('.fa-trash')
+const thumbText = document.querySelectorAll('.fa-thumbs-up')
+
+Array.from(deleteText).forEach((element)=>{
+    element.addEventListener('click', deleteRapper)
+})
+
+Array.from(thumbText).forEach((element)=>{
+    element.addEventListener('click', addLike)
+})
+
+async function deleteRapper(){
+    const sName = this.parentNode.childNodes[1].innerText
+    const bName = this.parentNode.childNodes[3].innerText
+    try{
+        const response = await fetch('deleteRapper', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'stageNameS': sName,
+              'birthNameS': bName
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function addLike(){
+    const sName = this.parentNode.childNodes[1].innerText
+    const bName = this.parentNode.childNodes[3].innerText
+    const tLikes = Number(this.parentNode.childNodes[5].innerText)
+    try{
+        const response = await fetch('addOneLike', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'stageNameS': sName,
+              'birthNameS': bName,
+              'likesS': tLikes
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json())
 
 
 app.get('/',(request, response)=>{
-    db.collection('rappers').find().toArray()
+    db.collection('rappers').find().sort({likes: -1}).toArray()
     .then(data => {
         response.render('index.ejs', { info: data })
     })
@@ -28,12 +28,30 @@ app.get('/',(request, response)=>{
 })
 
 app.post('/addRapper', (request, response) => {
-    db.collection('rappers').insertOne(request.body)
+    db.collection('rappers').insertOne({stageName: request.body.stageName,
+    birthName: request.body.birthName, likes: 0})
     .then(result => {
         console.log('Rapper Added')
         response.redirect('/')
     })
     .catch(error => console.error(error))
+})
+
+app.put('/addOneLike', (request, response) => {
+    db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS,likes: request.body.likesS},{
+        $set: {
+            likes:request.body.likesS + 1
+          }
+    },{
+        sort: {_id: -1},
+        upsert: true
+    })
+    .then(result => {
+        console.log('Added One Like')
+        response.json('Like Added')
+    })
+    .catch(error => console.error(error))
+
 })
 
 app.delete('/deleteRapper', (request, response) => {
